6b18a8ad40de0da6136973007a94336cb55d459e
Muhammad Rafid A <13515125@std.stei.itb.ac.id>
2018-03-17 15:29:45 +0700

    Change title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laray
+                    PPL II-2
                 </div>
 
                 <div class="links">
