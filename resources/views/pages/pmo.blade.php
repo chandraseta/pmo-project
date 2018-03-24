8656986384a51e372b21a31f7895b68d272bc03b
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-03-24 08:26:14 +0700

    Created layout to avoid boilerplate codes.

--- resources/views/pages/pmo.blade.php
+++ resources/views/pages/pmo.blade.php
@@ -1,20 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Databases | {{ config('app.name', 'PMO') }}</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-    <!-- Style -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
-</head>
-<body>
-    <h1>This is pmo page.</h1>
-</body>
-</html>
+@section('content')
+    <h1>This is PMO page with layout.</h1>
+@endsection
