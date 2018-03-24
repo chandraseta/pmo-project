321d4636044b00ada6ccf88d892c58afb9b9fabc
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-03-24 08:44:52 +0700

    Added color theme variables to _variables.scss.

--- resources/views/pages/pmo.blade.php
+++ resources/views/pages/pmo.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('header')
+    <nav class="navbar bg-primary navbar-dark">
+        <a class="navbar-brand" href="#">UPT Pengembangan Manusia dan Organisasi</a>
+    </nav>
+@endsection
+
 @section('content')
     <h1>This is PMO page with layout.</h1>
 @endsection
