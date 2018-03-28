b78f52a177bbd1dcac2208425fb2a9c3099454f3
Muhammad Rafid A <13515125@std.stei.itb.ac.id>
2018-03-28 14:38:48 +0700

    Change home routes

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@
 |
 */
 
-Route::get('/', 'PagesController@index');
-
-Route::get('/pages/pmo', 'PagesController@pmo');
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/pages/pmo', 'PagesController@pmo');
