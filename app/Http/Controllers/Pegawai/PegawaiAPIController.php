1516da931a7ff2d5284df01fd4636f07b85eda77
fidrafid@gmail.com <fidrafid@gmail.com>
2018-04-10 08:26:43 +0700

    Fix CRUD on Pegawai's Data

---
+++ app/Http/Controllers/Pegawai/PegawaiAPIController.php
@@ -0,0 +1,269 @@
+<?php
+
+
+namespace App\Http\Controllers\Pegawai;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Pegawai\APIBaseController as APIBaseController;
+use App\User;
+use App\Pegawai;
+use App\RiwayatPendidikan;
+use App\RiwayatPekerjaan;
+use App\DataKepegawaian;
+use Validator;
+use Illuminate\Support\Facades\Hash;
+
+
+class PegawaiAPIController extends APIBaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Pegawai::all();
+        return $this->sendResponse($posts->toArray(), 'Profiles retrieved successfully.');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+
+
+        $validator = Validator::make($input, [
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'nama' => 'required',
+            'nip' => 'required',
+            'tempat_lahir' => 'required',
+            'tanggal_lahir' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+
+        $find = User::where('username', $input['username'])->count();
+
+        if($find != 0){
+            return $this->sendError('Username Already Exist');
+        }
+
+        $postUser = User::create([
+            'username' => $input['username'],
+            'email' => $input['email'],
+            'password' => Hash::make($input['password']),
+        ]);
+
+        $user = User::where('username', $input['username'])->first();
+
+        $postPegawai = Pegawai::create([
+            'id_user' => $user->id,
+            'nama' => $input['nama'],
+            'nip' => $input['nip'],
+            'tempat_lahir' => $input['tempat_lahir'],
+            'tanggal_lahir' => $input['tanggal_lahir'],
+        ]);
+
+        $post = array_merge($postUser->toArray(), $postPegawai->toArray());
+
+        return $this->sendResponse($post, 'User created successfully.');
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Profile::find($id);
+
+
+        if (is_null($post)) {
+            return $this->sendError('Profile not found.');
+        }
+
+
+        return $this->sendResponse($post->toArray(), 'Profile retrieved successfully.');
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'nama' => 'required',
+            'nip' => 'required',
+            'tempat_lahir' => 'required',
+            'tanggal_lahir' => 'required',
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+        
+
+        $user = User::find($id);
+
+        if (is_null($user)) {
+            return $this->sendError('Profile not found.');
+        }
+
+
+        $find = User::where('username', $input['username']);
+
+        if($find->count() != 0 && $find->first()->username != $user->username){
+            return $this->sendError('Username Already Exist');
+        }
+
+
+        $pegawai = Pegawai::where('id_user', $id)->first();
+
+        $user->username = $input['username'];
+        $user->email = $input['email'];
+        $user->password = Hash::make($input['password']);
+        $pegawai->nama = $input['nama'];
+        $pegawai->nip = $input['nip'];
+        $pegawai->tempat_lahir = $input['tempat_lahir'];
+        $pegawai->tanggal_lahir = $input['tanggal_lahir'];
+
+
+        $pendidikan = RiwayatPendidikan::where('id_pegawai', $id);
+
+        if($pendidikan->count() > 0){
+            $pendidikan->delete();
+        }
+
+        for($i = 1; $i <= $input['pendidikan_counter']; $i++){
+            $postRiwayatPendidikan = RiwayatPendidikan::create([
+                'id_pegawai' => $id,
+                'nama_institusi' => $input['pendidikan_nama_institusi_' . $i],
+                'strata' => $input['pendidikan_strata_' . $i],
+                'jurusan' => $input['pendidikan_jurusan_' . $i],
+                'tahun_masuk' => $input['pendidikan_tahun_masuk_' . $i],
+                'tahun_keluar' => $input['pendidikan_tahun_keluar_' . $i],
+            ]);
+        }
+
+
+        $pekerjaan = RiwayatPekerjaan::where('id_pegawai', $id);
+
+        if($pekerjaan->count() > 0){
+            $pekerjaan->delete();
+        }
+
+        for($i = 1; $i <= $input['pekerjaan_counter']; $i++){
+            $postRiwayatPekerjaan = RiwayatPekerjaan::create([
+                'id_pegawai' => $id,
+                'nama_institusi' => $input['pekerjaan_nama_institusi_' . $i],
+                'posisi' => $input['pekerjaan_posisi_' . $i],
+                'tahun_masuk' => $input['pekerjaan_tahun_masuk_' . $i],
+                'tahun_keluar' => $input['pekerjaan_tahun_keluar_' . $i],
+            ]);
+        }
+
+
+        $kepegawaian = DataKepegawaian::where('id_pegawai', $id);
+
+        if($kepegawaian->count() > 0){
+            $kepegawaian->delete();
+        }
+
+        for($i = 1; $i <= $input['kepegawaian_counter']; $i++){
+            $postDataKepegawaian = DataKepegawaian::create([
+                'id_pegawai' => $id,
+                'kompetensi' => $input['kepegawaian_kompetensi_' . $i],
+                'unit_kerja' => $input['kepegawaian_unit_kerja_' . $i],
+                'posisi' => $input['kepegawaian_posisi_' . $i],
+                'tahun_masuk' => $input['kepegawaian_tahun_masuk_' . $i],
+                'tahun_keluar' => $input['kepegawaian_tahun_keluar_' . $i],
+            ]);
+        }
+
+
+        $user->save();
+        $pegawai->save();
+
+        $data = array_merge(
+            $user->toArray(),
+            $pegawai->toArray(),
+            $pendidikan->get()->toArray(),
+            $pekerjaan->get()->toArray(),
+            $kepegawaian->get()->toArray()
+        );
+
+
+        return $this->sendResponse($data, 'Profile updated successfully.');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (is_null($user)) {
+            return $this->sendError('Profile not found.');
+        }
+
+
+        $pendidikan = RiwayatPendidikan::where('id_pegawai', $id);
+
+        if($pendidikan->count() > 0){
+            $pendidikan->delete();
+        }
+
+
+        $pekerjaan = RiwayatPekerjaan::where('id_pegawai', $id);
+
+        if($pekerjaan->count() > 0){
+            $pekerjaan->delete();
+        }
+
+
+        $kepegawaian = DataKepegawaian::where('id_pegawai', $id);
+
+        if($kepegawaian->count() > 0){
+            $kepegawaian->delete();
+        }
+
+
+        $pegawai = Pegawai::where('id_user', $id);
+        $pegawai->delete();
+
+        $user->delete();
+
+
+        return $this->sendResponse($id, 'Tag deleted successfully.');
+    }
+}
