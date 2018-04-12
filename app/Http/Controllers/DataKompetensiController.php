17dd2e65e07097fc9e5e81add5536e4f38482799
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-04-12 21:46:10 +0700

    Added validation to DataKompetensiController update method.

--- app/Http/Controllers/DataKompetensiController.php
+++ app/Http/Controllers/DataKompetensiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\APIBaseController;
 use App\Kompetensi;
+use Validator;
 
 class DataKompetensiController extends APIBaseController
 {
@@ -77,7 +78,47 @@ class DataKompetensiController extends APIBaseController
      */
     public function update(Request $request, $id)
     {
-        $data = $request->all();
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+           'id_pegawai' => 'required',
+           'tanggal' => 'required',
+           'kognitif_efisiensi_kecerdasan' => 'required',
+           'kognitif_daya_nalar' => 'required',
+           'kognitif_daya_asosiasi' => 'required',
+           'kognitif_daya_analitis' => 'required',
+           'kognitif_daya_antisipasi' => 'required',
+           'kognitif_kemandirian_berpikir' => 'required',
+           'kognitif_fleksibilitas' => 'required',
+           'kognitif_daya_tangkap' => 'required',
+           'interaksional_penempatan_diri' => 'required',
+           'interaksional_percaya_diri' => 'required',
+           'interaksional_daya_kooperatif' => 'required',
+           'interaksional_penyesuaian_perasaan' => 'required',
+           'emosional_stabilitas_emosi' => 'required',
+           'emosional_toleransi_stres' => 'required',
+           'emosional_pengendalian_diri' => 'required',
+           'emosional_kemantapan_konsentrasi' => 'required',
+           'sikap_kerja_hasrat_berprestasi' => 'required',
+           'sikap_kerja_daya_tahan' => 'required',
+           'sikap_kerja_keteraturan_kerja' => 'required',
+           'sikap_kerja_pengerahan_energi_kerja' => 'required',
+           'manajerial_efektivitas_perencanaan' => 'required',
+           'manajerial_pengorganisasian_pelaksanaan' => 'required',
+           'manajerial_intensitas_pengarahan' => 'required',
+            'manajerial_kekuatan_pengawasan' => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());
+        }
+
+        $data = Kompetensi::find($id);
+
+        if (is_null($data)) {
+            $this->sendError('Data Kompetensi with id = '.$id.' is not found');
+        }
+
         return $this->sendResponse($data, 'Updated');
     }
 
