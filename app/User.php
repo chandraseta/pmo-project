557f82282a6ca2bb06d6ed9ed9c9409171ebd91f
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-04-15 01:03:23 +0700

    Added base for resetting user password

--- app/User.php
+++ app/User.php
@@ -26,4 +26,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token', 'created_at', 'updated_at',
     ];
+
+    public function sendPasswordResetNotification($token) {
+        $this->notify(new ResetPassword($token));
+    }
 }
