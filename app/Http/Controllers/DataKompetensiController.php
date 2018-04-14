b2025c083fd334243bffb3d528834e59e099010f
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-04-14 21:47:20 +0700

    Refactored DataKompetensiController.

--- app/Http/Controllers/DataKompetensiController.php
+++ app/Http/Controllers/DataKompetensiController.php
@@ -57,10 +57,10 @@ class DataKompetensiController extends APIBaseController
         $data = Kompetensi::find($id);
 
         if (is_null($data)) {
-            return $this->sendError('Data Kompetensi is not found.');
+            return $this->sendError('Data kompetensi tidak ditemukan.');
         }
 
-        return $this->sendResponse($data, 'Data Kompetensi retrieved successfully.');
+        return $this->sendResponse($data, 'Data kompetensi berhasil ditemukan.');
     }
 
     /**
@@ -85,75 +85,20 @@ class DataKompetensiController extends APIBaseController
     {
         $input = $request->all();
 
-        $validator = Validator::make($input, [
-           'id_pegawai' => 'required',
-           'tanggal' => 'required',
-           'kognitif_efisiensi_kecerdasan' => 'required',
-           'kognitif_daya_nalar' => 'required',
-           'kognitif_daya_asosiasi' => 'required',
-           'kognitif_daya_analitis' => 'required',
-           'kognitif_daya_antisipasi' => 'required',
-           'kognitif_kemandirian_berpikir' => 'required',
-           'kognitif_fleksibilitas' => 'required',
-           'kognitif_daya_tangkap' => 'required',
-           'interaksional_penempatan_diri' => 'required',
-           'interaksional_percaya_diri' => 'required',
-           'interaksional_daya_kooperatif' => 'required',
-           'interaksional_penyesuaian_perasaan' => 'required',
-           'emosional_stabilitas_emosi' => 'required',
-           'emosional_toleransi_stres' => 'required',
-           'emosional_pengendalian_diri' => 'required',
-           'emosional_kemantapan_konsentrasi' => 'required',
-           'sikap_kerja_hasrat_berprestasi' => 'required',
-           'sikap_kerja_daya_tahan' => 'required',
-           'sikap_kerja_keteraturan_kerja' => 'required',
-           'sikap_kerja_pengerahan_energi_kerja' => 'required',
-           'manajerial_efektivitas_perencanaan' => 'required',
-           'manajerial_pengorganisasian_pelaksanaan' => 'required',
-           'manajerial_intensitas_pengarahan' => 'required',
-            'manajerial_kekuatan_pengawasan' => 'required'
-        ]);
-
+        $validator = $this->validateDataKompetensi($input);
         if($validator->fails()){
-            return $this->sendError('Validation Error.', $validator->errors());
+            return $this->sendError('Gagal menyimpan data kompetensi.', $validator->errors());
         }
 
         $data = Kompetensi::find($id);
-
         if (is_null($data)) {
-            $this->sendError('Data Kompetensi with id = '.$id.' is not found');
+            $this->sendError('Data Kompetensi dengan id = '.$id.' tidak ditemukan.');
         }
 
-        $data->id_pegawai = $input['id_pegawai'];
-        $data->tanggal = $input['tanggal'];
-        $data->kognitif_efisiensi_kecerdasan = $input['kognitif_efisiensi_kecerdasan'];
-        $data->kognitif_daya_nalar = $input['kognitif_daya_nalar'];
-        $data->kognitif_daya_asosiasi = $input['kognitif_daya_asosiasi'];
-        $data->kognitif_daya_analitis = $input['kognitif_daya_analitis'];
-        $data->kognitif_daya_antisipasi = $input['kognitif_daya_antisipasi'];
-        $data->kognitif_kemandirian_berpikir = $input['kognitif_kemandirian_berpikir'];
-        $data->kognitif_fleksibilitas = $input['kognitif_fleksibilitas'];
-        $data->kognitif_daya_tangkap = $input['kognitif_daya_tangkap'];
-        $data->interaksional_penempatan_diri = $input['interaksional_penempatan_diri'];
-        $data->interaksional_percaya_diri = $input['interaksional_percaya_diri'];
-        $data->interaksional_daya_kooperatif = $input['interaksional_daya_kooperatif'];
-        $data->interaksional_penyesuaian_perasaan = $input['interaksional_penyesuaian_perasaan'];
-        $data->emosional_stabilitas_emosi = $input['emosional_stabilitas_emosi'];
-        $data->emosional_toleransi_stres = $input['emosional_toleransi_stres'];
-        $data->emosional_pengendalian_diri = $input['emosional_pengendalian_diri'];
-        $data->emosional_kemantapan_konsentrasi = $input['emosional_kemantapan_konsentrasi'];
-        $data->sikap_kerja_hasrat_berprestasi = $input['sikap_kerja_hasrat_berprestasi'];
-        $data->sikap_kerja_daya_tahan = $input['sikap_kerja_daya_tahan'];
-        $data->sikap_kerja_keteraturan_kerja = $input['sikap_kerja_keteraturan_kerja'];
-        $data->sikap_kerja_pengerahan_energi_kerja = $input['sikap_kerja_pengerahan_energi_kerja'];
-        $data->manajerial_efektivitas_perencanaan = $input['manajerial_efektivitas_perencanaan'];
-        $data->manajerial_pengorganisasian_pelaksanaan = $input['manajerial_pengorganisasian_pelaksanaan'];
-        $data->manajerial_intensitas_pengarahan = $input['manajerial_intensitas_pengarahan'];
-        $data->manajerial_kekuatan_pengawasan = $input['manajerial_kekuatan_pengawasan'];
-
+        $data = $this->updateDataKompetensi($data, $input);
         $data->save();
 
-        return $this->sendResponse($data, 'Data Kompetensi successfully updated.');
+        return $this->sendResponse($data, 'Data kompetensi berhasil disimpan.');
     }
 
     /**
@@ -165,5 +110,69 @@ class DataKompetensiController extends APIBaseController
     public function destroy($id)
     {
         //
+    }
+
+    private function validateDataKompetensi($input) {
+        return Validator::make($input, [
+            'id_pegawai' => 'required',
+            'tanggal' => 'required',
+            'kognitif_efisiensi_kecerdasan' => 'required',
+            'kognitif_daya_nalar' => 'required',
+            'kognitif_daya_asosiasi' => 'required',
+            'kognitif_daya_analitis' => 'required',
+            'kognitif_daya_antisipasi' => 'required',
+            'kognitif_kemandirian_berpikir' => 'required',
+            'kognitif_fleksibilitas' => 'required',
+            'kognitif_daya_tangkap' => 'required',
+            'interaksional_penempatan_diri' => 'required',
+            'interaksional_percaya_diri' => 'required',
+            'interaksional_daya_kooperatif' => 'required',
+            'interaksional_penyesuaian_perasaan' => 'required',
+            'emosional_stabilitas_emosi' => 'required',
+            'emosional_toleransi_stres' => 'required',
+            'emosional_pengendalian_diri' => 'required',
+            'emosional_kemantapan_konsentrasi' => 'required',
+            'sikap_kerja_hasrat_berprestasi' => 'required',
+            'sikap_kerja_daya_tahan' => 'required',
+            'sikap_kerja_keteraturan_kerja' => 'required',
+            'sikap_kerja_pengerahan_energi_kerja' => 'required',
+            'manajerial_efektivitas_perencanaan' => 'required',
+            'manajerial_pengorganisasian_pelaksanaan' => 'required',
+            'manajerial_intensitas_pengarahan' => 'required',
+            'manajerial_kekuatan_pengawasan' => 'required'
+        ]);
+    }
+
+    private function updateDataKompetensi($oldData, $newDataInput) {
+        $newData = $oldData;
+
+        $newData->id_pegawai = $newDataInput['id_pegawai'];
+        $newData->tanggal = $newDataInput['tanggal'];
+        $newData->kognitif_efisiensi_kecerdasan = $newDataInput['kognitif_efisiensi_kecerdasan'];
+        $newData->kognitif_daya_nalar = $newDataInput['kognitif_daya_nalar'];
+        $newData->kognitif_daya_asosiasi = $newDataInput['kognitif_daya_asosiasi'];
+        $newData->kognitif_daya_analitis = $newDataInput['kognitif_daya_analitis'];
+        $newData->kognitif_daya_antisipasi = $newDataInput['kognitif_daya_antisipasi'];
+        $newData->kognitif_kemandirian_berpikir = $newDataInput['kognitif_kemandirian_berpikir'];
+        $newData->kognitif_fleksibilitas = $newDataInput['kognitif_fleksibilitas'];
+        $newData->kognitif_daya_tangkap = $newDataInput['kognitif_daya_tangkap'];
+        $newData->interaksional_penempatan_diri = $newDataInput['interaksional_penempatan_diri'];
+        $newData->interaksional_percaya_diri = $newDataInput['interaksional_percaya_diri'];
+        $newData->interaksional_daya_kooperatif = $newDataInput['interaksional_daya_kooperatif'];
+        $newData->interaksional_penyesuaian_perasaan = $newDataInput['interaksional_penyesuaian_perasaan'];
+        $newData->emosional_stabilitas_emosi = $newDataInput['emosional_stabilitas_emosi'];
+        $newData->emosional_toleransi_stres = $newDataInput['emosional_toleransi_stres'];
+        $newData->emosional_pengendalian_diri = $newDataInput['emosional_pengendalian_diri'];
+        $newData->emosional_kemantapan_konsentrasi = $newDataInput['emosional_kemantapan_konsentrasi'];
+        $newData->sikap_kerja_hasrat_berprestasi = $newDataInput['sikap_kerja_hasrat_berprestasi'];
+        $newData->sikap_kerja_daya_tahan = $newDataInput['sikap_kerja_daya_tahan'];
+        $newData->sikap_kerja_keteraturan_kerja = $newDataInput['sikap_kerja_keteraturan_kerja'];
+        $newData->sikap_kerja_pengerahan_energi_kerja = $newDataInput['sikap_kerja_pengerahan_energi_kerja'];
+        $newData->manajerial_efektivitas_perencanaan = $newDataInput['manajerial_efektivitas_perencanaan'];
+        $newData->manajerial_pengorganisasian_pelaksanaan = $newDataInput['manajerial_pengorganisasian_pelaksanaan'];
+        $newData->manajerial_intensitas_pengarahan = $newDataInput['manajerial_intensitas_pengarahan'];
+        $newData->manajerial_kekuatan_pengawasan = $newDataInput['manajerial_kekuatan_pengawasan'];
+
+        return $newData;
     }
 }
