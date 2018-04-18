f8d8a6067ea67160ae50ae099d91687d762d1b68
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-04-18 20:58:02 +0700

    Refactored data pegawai API to denormalized data pegawai.

--- resources/assets/js/components/PMOMainPage.vue
+++ resources/assets/js/components/PMOMainPage.vue
@@ -263,22 +263,6 @@
             getPegawai: function () {
                 axios.get('/api/pegawai-denormalized')
                     .then(response => {
-//                        this.dataPegawai = response.data.data;
-//                        this.dataPegawai.forEach(function (row, index, array) {
-//                            if (row.data_kepegawaians.length > 0) {
-//                                let data_kepegawaian = row.data_kepegawaians[row.data_kepegawaians.length - 1];
-//                                array[index].unit_kerja = data_kepegawaian.unit_kerja;
-//                                array[index].kompetensi = data_kepegawaian.kompetensi;
-//                                array[index].jabatan = data_kepegawaian.posisi;
-//                                array[index].tahun_masuk = data_kepegawaian.tahun_masuk;
-//                            }
-//
-//                            if (row.riwayat_pendidikans.length > 0) {
-//                                array[index].strata = row.riwayat_pendidikans[row.riwayat_pendidikans.length - 1].strata;
-//                            }
-//                        });
-//                        this.columns = this.$options.dataPegawaiColumns;
-//                        this.rows = this.dataPegawai;
                         this.dataPegawai = response.data.data;
                         this.rows = this[this.currentTab];
                     })
