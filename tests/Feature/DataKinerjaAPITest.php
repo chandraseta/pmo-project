9fe0202f3cac6aaeec8faa1edb6ca06ff8398e41
Alkhwarizm <iqbal.alkhowarizmi@gmail.com>
2018-04-19 13:07:20 +0700

    Created test for DataKinerja.

---
+++ tests/Feature/DataKinerjaAPITest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Kinerja;
+use App\Kompetensi;
+use App\Pegawai;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DataKinerjaAPITest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $user;
+    private $baseUri = 'api/kinerja';
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+    }
+
+    public function testItFetchesDataKinerja() {
+        $data = Kinerja::all();
+        $response = $this->actingAs($this->user)
+            ->get($this->baseUri)
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+                'data' => $data->toArray()
+            ]);
+    }
+
+    public function testItFetchesParticularDataKinerja() {
+        $randomData = Kinerja::inRandomOrder()->first();
+        $existingId = $randomData->id_kinerja;
+        $uri = $this->baseUri.'/'.$existingId;
+        $response = $this->actingAs($this->user)
+            ->get($uri)
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => $randomData->toArray()
+            ]);
+
+        $nonExistingId = Kinerja::all()->max('id_kinerja') + 1;
+        $uri = $this->baseUri.'/'.$nonExistingId;
+        $response = $this->actingAs($this->user)
+            ->get($uri)
+            ->assertStatus(404);
+
+    }
+
+    public function testItStoresDataKinerjaToDatabase() {
+        $method = 'POST';
+        $uri = $this->baseUri;
+        $data = factory(Kinerja::class)->make();
+        unset($data->id_pegawai);
+
+        $payload = $data;
+        $response = $this->actingAs($this->user)
+            ->json($method, $uri, $payload->toArray())
+            ->assertStatus(404);
+
+        $randomUser = Pegawai::inRandomOrder()->first();
+        $data->nip = $randomUser->nip;
+
+        $payload = $data->toArray();
+        unset($data->nip);
+        $response = $this->actingAs($this->user)
+            ->json($method, $uri, $payload)
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => $data->toArray()
+            ]);
+
+        $this->assertDatabaseHas('kinerja', $data->toArray());
+    }
+
+    public function testItUpdatesDataKinerja() {
+        $data = Kinerja::inRandomOrder()->first();
+        $newData = factory(Kinerja::class)->make();
+        $newData->id_kinerja = $data->id_kinerja;
+        $newData->id_pegawai = $data->id_pegawai;
+
+        $method = 'PUT';
+        $uri = $this->baseUri.'/'.$newData->id_kinerja;
+        $payload = $newData->toArray();
+        $response = $this->actingAs($this->user)
+            ->json($method, $uri, $payload)
+            ->assertStatus(200)
+            ->assertJson([
+                'data' => $payload
+            ]);
+
+        $nonExistId = Kinerja::all()->max('id_kinerja') + 1;
+        $newData->id_kinerja = $nonExistId;
+        $uri = $this->baseUri.'/'.$newData->id_kinerja;
+        $payload = $newData->toArray();
+        $response = $this->actingAs($this->user)
+            ->json($method, $uri, $payload)
+            ->assertStatus(404);
+    }
+}
