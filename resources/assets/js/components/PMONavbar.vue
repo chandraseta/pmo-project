e61e677a1b63b504193d6dc352a34424093f1c21
rionaldichandraseta <rionaldi.chandraseta@gmail.com>
2018-04-27 12:28:56 +0700

    Modified PMO & Admin's navbar

--- resources/assets/js/components/PMONavbar.vue
+++ resources/assets/js/components/PMONavbar.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-md bg-primary navbar-dark">
-        <a class="navbar-brand font-weight-bold" href="#">UPT PMO</a>
+        <a class="navbar-brand font-weight-bold" href="/pages">UPT PMO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
